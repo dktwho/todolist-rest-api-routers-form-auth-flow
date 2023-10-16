925ebdf66a50456eb2dc035b92d0132b210717be
Alexander Korobov <dktwho@mail.ru>
2023-10-16 11:10:30 +0300

    add Navigate to 404 for view url 404

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {Login} from '../features/Login/Login'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 
 function App() {
@@ -37,7 +37,9 @@ function App() {
                 <Routes>
                     <Route path={'/'} element={<TodolistsList/>}/>
                     <Route path={'/login'} element={<Login/>}/>
-                    <Route path='*' element={<h1>404: PAGE NOT FOUND</h1>} />
+
+                    <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path='*' element={<Navigate to={'/404'}/>}/>
                 </Routes>
             </Container>
         </div>
