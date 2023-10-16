ff84f6f2df49bf897bd852461c9d2bf95fddb1db
Alexander Korobov <dktwho@mail.ru>
2023-10-16 11:01:19 +0300

    add browser-router and routes with route for login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,8 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from '../features/Login/Login'
+import {Routes, Route} from 'react-router-dom'
 
 
 function App() {
@@ -32,7 +34,12 @@ function App() {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                </Routes>
+
+
             </Container>
         </div>
     )
