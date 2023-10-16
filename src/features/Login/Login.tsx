be0d06f083091e4ae3c23aa8fc157007672ce974
Alexander Korobov <dktwho@mail.ru>
2023-10-16 12:25:07 +0300

    validate errors for length password modified

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -27,8 +27,11 @@ export const Login = () => {
                 errors.email = 'Required'
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address'
-            } else if (values.password.length < 3) {
-                errors.password = 'Password length must be more 3 symbols'
+            }
+            if (!values.password) {
+                errors.password = 'Required'
+            } else if(values.password.length < 5) {
+                errors.password = 'Password length must be more 5 symbols'
             }
             return errors
         },
