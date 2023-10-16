d20237eea7d2037b4257a29cde929c5f000f54d7
Alexander Korobov <dktwho@mail.ru>
2023-10-16 11:44:56 +0300

    add handlechange and hadlesubmit for formik, modified Textfield

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,7 +7,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useFormik } from 'formik';
+import {useFormik} from 'formik';
 
 export const Login = () => {
     const formik = useFormik({
@@ -23,28 +23,40 @@ export const Login = () => {
 
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
-            <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href={'https://social-network.samuraijs.com/'}
-                           target={'_blank'}> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                    <TextField label="Email" margin="normal"/>
-                    <TextField type="password" label="Password"
-                               margin="normal"
-                    />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
-            </FormControl>
+            <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                    <FormLabel>
+                        <p>To log in get registered
+                            <a href={'https://social-network.samuraijs.com/'}
+                               target={'_blank'}> here
+                            </a>
+                        </p>
+                        <p>or use common test account credentials:</p>
+                        <p>Email: free@samuraijs.com</p>
+                        <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField label="Email"
+                                   margin="normal"
+                                   name='email'
+                                   onChange={formik.handleChange}
+                                   value={formik.values.email}
+                        />
+                        <TextField type="password"
+                                   label="Password"
+                                   margin="normal"
+                                   name='password'
+                                   onChange={formik.handleChange}
+                                   value={formik.values.password}
+                        />
+                        <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </FormControl>
+            </form>
+
         </Grid>
     </Grid>
 }
