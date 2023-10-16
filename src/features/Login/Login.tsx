549685e24a71446989b10d7da5beb9aa5c4bdfe1
Alexander Korobov <dktwho@mail.ru>
2023-10-16 11:50:50 +0300

    add to component Login function validate inside formik and typed

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -9,12 +9,26 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from 'formik';
 
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
 export const Login = () => {
     const formik = useFormik({
         initialValues: {
             email: '',
             password: '',
             rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            return errors
         },
         onSubmit: values => {
             alert(JSON.stringify(values));
